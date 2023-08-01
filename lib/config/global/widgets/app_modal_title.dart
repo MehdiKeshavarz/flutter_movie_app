d01f7cdf0e2b8cf7c754a11538fafe1e36aa9d01
Mehdi Keshavarz <mehdi2019keshavarz@gmail.com>
2023-08-01 21:54:49 +0330

    create widget search & filter

---
+++ lib/config/global/widgets/app_modal_title.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/app_color.dart';
+import '../../theme/app_theme.dart';
+
+class AppModalTitle extends StatelessWidget {
+  final String modalTitle;
+  const AppModalTitle({super.key, required this.modalTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SliverAppBar(
+      automaticallyImplyLeading: false,
+      pinned: true,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(56),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: Text(
+                modalTitle,
+                textAlign: TextAlign.center,
+                style: theme.textTheme.headlineMedium!.copyWith(
+                  color: AppColors.error,
+                ),
+              ),
+            ),
+            Divider(
+              color: AppDynamicColorBuilder.getDark3AndGrey200(context),
+              thickness: 1,
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/feature/explore/widgets/search_and_filter_widget.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:move_app/config/global/constant/image_route.dart';
+import 'package:move_app/config/global/utils/show_model.dart';
+import 'package:move_app/config/theme/app_theme.dart';
+
+import '../../../config/theme/app_color.dart';
+
+class SearchAndFilter extends StatelessWidget {
+  const SearchAndFilter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      sliver: SliverToBoxAdapter(
+        child: Row(
+          children: [
+            SearchBox(),
+            SizedBox(width: 6),
+            FilterButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FilterButton extends StatelessWidget {
+  const FilterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => showModelApp(
+          context: context,
+          modalTitle: 'sort & filter',
+          primaryButtonTitle: 'Apply',
+          secondaryButtonTitle: 'Reset ',
+          initChildSize: 0.7,
+          minChildSize: 0.4,
+          maxChildSize: 0.9,
+          mainModalContent: const Card(
+            color: Colors.transparent,
+            elevation: 0,
+          )),
+      child: Container(
+        height: 56,
+        width: 56,
+        padding: const EdgeInsets.all(16),
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          color: AppColors.redTransparent.withOpacity(0.1),
+        ),
+        child: SvgPicture.asset(AppImageRoute.iconFilter),
+      ),
+    );
+  }
+}
+
+class SearchBox extends StatefulWidget {
+  const SearchBox({super.key});
+
+  @override
+  State<SearchBox> createState() => _SearchBoxState();
+}
+
+class _SearchBoxState extends State<SearchBox> {
+  final FocusNode searchFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    searchFocusNode.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchFocusNode.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Expanded(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppDynamicColorBuilder.getGrey100AndDark2(context),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+              color: searchFocusNode.hasFocus
+                  ? theme.primaryColor
+                  : AppDynamicColorBuilder.getGrey100AndDark2(context),
+              width: 1),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: TextField(
+          focusNode: searchFocusNode,
+          style: theme.textTheme.bodyMedium!.copyWith(
+              fontWeight: FontWeight.w600,
+              color: AppDynamicColorBuilder.getGrey900AndWhite(context)),
+          decoration: InputDecoration(
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: 'search',
+            hintStyle: theme.textTheme.bodyMedium!.copyWith(
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+              color: AppDynamicColorBuilder.getGrey600AndGrey400(context),
+            ),
+            icon: SvgPicture.asset(
+              AppImageRoute.iconSearch,
+              color: searchFocusNode.hasFocus
+                  ? theme.primaryColor
+                  : AppDynamicColorBuilder.getGrey600AndGrey400(context),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
